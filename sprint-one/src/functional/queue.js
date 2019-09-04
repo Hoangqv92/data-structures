c5aa8474d322236f74c3f6200f8449bb7b751012
Vinh <hoangqv92@gmail.com>
2019-09-04 11:53:19 -0700

    first commit finished functional queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
+  // creeate variable for count(placeholder) and tail(end of placeholder)
+  var count = 0;
   var someInstance = {};
+  var tail = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +10,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add to the count(adds to the top of the Sandwich)
+    if (value) {
+      storage[count] = value;
+      count++;
+    }
   };
 
   someInstance.dequeue = function() {
+    // removes the tail end of count(removes end of Sandwich)
+    if (count - tail === 0) {
+      return undefined;
+    }
+    var results = storage[tail];
+    delete storage[tail];
+    tail++;
+    return results;
   };
 
   someInstance.size = function() {
+    // Subracts total new count with new tail to get total (returns back to OG size)
+    return count - tail;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,4 +1,5 @@
 var Stack = function() {
+  var count = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,12 +7,24 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // add count and use count to store value in storage;
+    count++;
+    storage[count] = value;
   };
 
   someInstance.pop = function() {
+    // removes value at top count in storage and returns value before top count
+    var results = storage[count];
+    delete storage[count];
+    if (count) {
+      count--;
+    }
+    return results;
   };
 
   someInstance.size = function() {
+    // returns the count;
+    return count;
   };
 
   return someInstance;
